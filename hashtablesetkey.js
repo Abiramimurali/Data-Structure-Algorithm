4bebc40bf33fadb1b7f235ddb2da63cb890ab6e2
ABIRAMI M <53057686+Abiramimurali@users.noreply.github.com>
2020-09-06 11:34:41 -0700

    Add files via upload

---
+++ hashtablesetkey.js
@@ -0,0 +1,13 @@
+// set(key,value){
+//     let address=this.Hash(key);
+//     console.log(address)
+//     //console.log(this.data[address]=9)
+//     console.log(!this.data[address])
+//     if(!this.data[address])
+//     {
+//         this.data[address]=[];
+//         this.data[address].push([key,value])
+//         console.log(this.data)
+//     }
+// }
+//---------------collision-------------------------------
